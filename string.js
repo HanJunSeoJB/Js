2dd0303647b617bed591193cd932bd0183ec9fa9
HanJunSeo <hjs123@jbnu.ac.kr>
2020-09-17 01:51:41 +0900

    practice string

---
+++ string.js
@@ -0,0 +1,6 @@
+var letter = 'Anthony Burgess (1917-93) was an English novelist, critic, and man of letters whose fictional explorations of modern dilemmas combine wit, moral earnestness, and a note of the bizarre. His A Clockwork...'
+var word = letter.split(' ');
+var i = 0;
+for(i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
